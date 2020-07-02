5e29d3de0aa1bdf0fd4e387a1f14e5c492b835fe
Quinn <quinnvoker@gmail.com>
2020-07-02 21:27:35 +0000

    added merge stretch exercise

---
+++ merge.js
@@ -0,0 +1,11 @@
+const merge = function(arrayA, arrayB) {
+  let result = arrayA.slice();
+  for (let element of arrayB) {
+    result.push(element);
+  }
+  return result.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
