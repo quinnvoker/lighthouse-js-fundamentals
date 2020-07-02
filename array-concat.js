7a0ba175f038702bd3a3ad995e2821f3864150fa
Quinn <quinnvoker@gmail.com>
2020-07-02 17:41:00 +0000

    added function to concatenate arrays

---
+++ array-concat.js
@@ -0,0 +1,12 @@
+const concat = function(arrayA, arrayB) {
+  let result = arrayA.slice();
+  for (let element of arrayB) {
+    result.push(element);
+  }
+  return result;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
