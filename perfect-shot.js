39cfef5bf36aaa8abce58527d8db6199c99214c3
Quinn <quinnvoker@gmail.com>
2020-07-02 02:37:20 +0000

    fixed indentation in perfect shot after modifying it for challenge

--- perfect-shot.js
+++ perfect-shot.js
@@ -2,19 +2,19 @@ const finalPosition = function (moves) {
   let position = [0, 0];
   for (let move of moves) {
     switch (move) {
-    case 'north':
-      position[1]++;
-      break;
-    case 'south':
-      position[1]--;
-      break;
-    case 'east':
-      position[0]++;
-      break;
-    case 'west':
-      position[0]--;
-      break;
-    }
+      case 'north':
+        position[1]++;
+        break;
+      case 'south':
+        position[1]--;
+        break;
+      case 'east':
+        position[0]++;
+        break;
+      case 'west':
+        position[0]--;
+        break;
+      }
   }
   return position;
 } 
